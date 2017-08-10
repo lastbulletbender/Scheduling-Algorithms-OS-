1c087292590a428def86f31eb7f343f6f5684144
lastbulletbender <chattopadhyayabhrajit@gmail.com>
2017-08-10 20:29:44 +0530

    Added the files.

---
+++ priorityScheduling.c
@@ -0,0 +1,90 @@
+/*PRIORITY SCHEDULING ALGORITHM IMPLEMENTED BY ABHRAJIT CHATTOPADHYAY*/
+ 
+#include<stdio.h>
+#include<stdlib.h>
+int totalWaitingTime;
+int totalTurnAroundTime;
+ 
+typedef struct process_struct{
+    int id;
+    int start_flag; //Necessary only if arrival time is different
+    int time_left; //How much work is left
+    int waiting_time; //How much time the process is waiting while other processes are working
+    int end_flag; // Unneccessary in this program
+    int turn_around_time;// Burst time + Completion Time (If arrival times are 0 for all processes))
+    int priority;
+    int burst_time;// Time required to complete the process
+}pro;
+ 
+ 
+void changeWaitingTime(pro process[],int n,int i,int wt){
+    /* This function changes the waiting time for all the processes except
+       the one which is processing.
+       Here i is the processing process, j is used to go through all the process
+       wt is the value, waiting_time has to be increased by*/
+   
+    for(int j=0;j<n;j++){
+        if(process[j].time_left!=0){//Put &&process[j].start_flag==1 to have different arrival time
+            if(j!=i) //If it is not the processing process
+                process[j].waiting_time+=wt;}}}
+ 
+ 
+void priorityScheduling(pro process[],int n){
+pro temp;
+    for(int i =0;i<n;i++){
+	for(int j=0;j<n;j++){
+		if(process[i].priority==process[j].priority){
+			if(process[i].id<process[j].id){
+				temp = process[i];
+			process[i] = process[j];
+			process[j] = temp;}}
+		if(process[i].priority>process[j].priority){
+			temp = process[i];
+			process[i] = process[j];
+			process[j] = temp;}}}
+       
+	
+
+    for(int i = 0;i<n;i++){
+	
+        //process[i].start_flag = 1;        Remove comment to have different arrival time
+        changeWaitingTime(process,n,i,process[i].time_left);
+       
+        /*In First Come First Serve, the process are completed at once*/
+       
+        printf("Process[%d] worked on for %d second(s)\n",i,process[i].time_left); //So, all of time_left is used
+        process[i].time_left = 0; //Since work is done, time_left is set to 0
+        totalWaitingTime += process[i].waiting_time; //Used to calculate average waiting time later
+        printf("Total Waiting time of process[%d] is %d\n",i,process[i].waiting_time); //Total waiting time of          current process
+        }
+       
+        }
+ 
+int main(){
+    static pro process[6]; //used static to set default values of struct members as 0
+    int n = sizeof(process)/sizeof(process[0]); //to get the number of elements in array
+    printf("Enter process burst_times & priority : \n");
+   
+   
+    /*Getting Burst values from the user */
+   
+    for(int i=0;i<n;i++){
+        scanf("%d %d",&process[i].burst_time,&process[i].priority);
+        process[i].time_left = process[i].burst_time; //Initally, both will be same
+	process[i].id = i;
+        }
+	
+           
+    priorityScheduling(process,n);
+   
+   
+    printf("Process\tBurstTime\tWaitingTime\tTurnAroundTime\tCompletionTime\n"); //Table printing
+    for(int i =0;i<n;i++){
+        process[i].turn_around_time = process[i].burst_time+process[i].waiting_time;
+        totalTurnAroundTime+=process[i].turn_around_time;
+        printf("p[%d]\t%d\t\t%d\t\t%d\t\t%d\n",process[i].id,process[i].burst_time,process[i].waiting_time,process[i].turn_around_time,process[i].turn_around_time);}
+    printf("\nAverage waiting time is %f\n",((float)totalWaitingTime/n));
+    printf("\nAverage turn around time is %f\n",((float)totalTurnAroundTime/n));
+   
+ 
+    return 0;}
---
+++ roundRobin.c
@@ -0,0 +1,86 @@
+/*Round Robin Algorithm implemented by Abhrajit Chattopadhyay*/
+ 
+#include<stdio.h>
+#define quantum 2
+int totalWaitingTime;
+int totalTurnAroundTime;
+ 
+ 
+typedef struct process_struct{
+    int start_flag;
+    int time_left;
+    int burst_time;
+    int waiting_time;
+    int end_flag;
+    int completion_time;
+    int turn_around_time;
+}pro;
+ 
+ 
+ 
+ 
+ 
+void changeWaitingTime(pro process[],int n,int i,int wt){
+   
+    for(int j=0;j<n;j++){
+        if(process[j].time_left!=0){//remove && statement for same arrival time  &&process[j].start_flag==1
+            if(j!=i)
+                process[j].waiting_time+=wt;}}}
+ 
+ 
+void roundRobin(pro process[],int n){
+    int end_flag=0;
+   
+    while(1){
+       
+        for(int i =0;i<n;i++){
+            process[i].start_flag=1;
+            if(process[i].end_flag!=1){
+                    if(process[i].time_left/quantum!=0){
+                        printf("Process[%d], worked on for %d second(s)\n",i,quantum);
+                        process[i].time_left-=quantum;
+                        process[i].completion_time+=quantum;
+                        changeWaitingTime(process,n,i,quantum);
+                        }
+                    else if(process[i].time_left%quantum!=0){
+                        printf("Process[%d], worked on for %d second\n",i,process[i].time_left%quantum);
+                        changeWaitingTime(process,n,i,process[i].time_left%quantum);
+                        process[i].completion_time+=process[i].time_left%quantum;
+                        process[i].time_left-=process[i].time_left%quantum;
+                       
+                        }
+                    else{
+                        printf("Process[%d] has ended!",i);
+                        printf("\nTotal wating time for process[%d] is %d\n",i,process[i].waiting_time);
+                        totalWaitingTime+=process[i].waiting_time;
+                        process[i].end_flag=1;;
+                        end_flag++;
+                        }}
+                       
+                }
+        if(end_flag>=n){
+               
+                break;}
+               
+                }
+}
+                       
+               
+ 
+int main(){
+    static pro process[5];
+    int n = sizeof(process)/sizeof(process[0]);
+    printf("Enter process time_lefts : \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&process[i].burst_time);
+        process[i].time_left = process[i].burst_time;
+    }  
+    roundRobin(process,n);
+    printf("Process\tBurstTime\tWaitingTime\tTurnAroundTime\tCompletionTime\n");
+    for(int i =0;i<n;i++){
+        process[i].turn_around_time = process[i].burst_time+process[i].waiting_time;
+        totalTurnAroundTime+=process[i].turn_around_time;
+        printf("p[%d]\t%d\t\t%d\t\t%d\t\t%d\n",i,process[i].burst_time,process[i].waiting_time,process[i].turn_around_time,process[i].turn_around_time);}
+    printf("\nAverage waiting time is %f\n",((float)totalWaitingTime/n));
+    printf("\nAverage turn around time is %f\n",((float)totalTurnAroundTime/n));
+    return 0;}
